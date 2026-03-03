56d93e28ed3303b932fd2629e972fa0cce246f70
Islam Adel Kadous <48600816+islamadelkadous@users.noreply.github.com>
2026-03-04 02:10:28 +0300

    Create binary-tree-diameter.py

---
+++ python/binary-tree-diameter.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def findLongest(root: Optional[TreeNode]) -> int:
+            nonlocal res
+            if not root:
+                return 0
+            
+            maxL = findLongest(root.left)
+            maxR = findLongest(root.right)
+
+            res = max(res, maxL + maxR)
+
+            return 1 + max(maxL, maxR)
+        
+        findLongest(root)
+        return res
+
+        
